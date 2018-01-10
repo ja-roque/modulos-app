bbb486902473433054de5413c01f9aba642f8afe
Unknown <jarb95@gmail.com>
2018-01-10 11:18:13 -0600

    Added reveal js as presentation engine.
    Also added animated background to the main content section.

--- modulos-liderazgo/src/app/auth.service.ts
+++ modulos-liderazgo/src/app/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
+
 
 @Injectable()
 export class AuthService {
--- modulos-liderazgo/src/app/landing-screen/landing-screen.component.ts
+++ modulos-liderazgo/src/app/landing-screen/landing-screen.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-landing-screen',
   templateUrl: './landing-screen.component.html',
