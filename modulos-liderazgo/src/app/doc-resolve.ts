346058726c0fdf32759a23ee7b17a74fa61cea78
Armando Roque <roque510@hotmail.com>
2018-02-02 00:25:37 -0600

    data service in resolves

--- modulos-liderazgo/src/app/doc-resolve.ts
+++ modulos-liderazgo/src/app/doc-resolve.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { DocfetchService } from './docfetch.service';
+import { DataService } from './data.service';
 
 @Injectable()
 export class DocResolve implements Resolve<any> {
+	num = 1;
 
-  constructor(private docFetchService: DocfetchService) {}
+  constructor(private docFetchService: DocfetchService,private data: DataService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
+
+  	this.data.currentSession.subscribe(value => {
+  		this.num = value;
+  	})
     // return this.docFetchService.getDoc((route.paramMap.get('id')));
-    return this.docFetchService.getDoc(1);
+    return this.docFetchService.getDoc(this.num);
   }
 }
 
