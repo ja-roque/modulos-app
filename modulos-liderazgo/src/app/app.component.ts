c45485f8ef3a2001d92d5e205bd60d7b78971c5c
Unknown <jarb95@gmail.com>
2018-02-18 22:43:52 -0600

    Fix location detection

--- modulos-liderazgo/src/app/app.component.ts
+++ modulos-liderazgo/src/app/app.component.ts
@@ -9,6 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Modulos de Liderazgo';
 
-  // This extracts the forwarded subdomain from Godaddy as github pages root directory is set to sub-directory a normal CNAME record cannot be set.
-  company = window.location.ancestorOrigins[0].split('.')[0].split('/')[2];
+  company = window.location.host.split( '.' )[0];
 }
