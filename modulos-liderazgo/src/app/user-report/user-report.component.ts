89e5266c0f7bc05550cbc39210083697b6bd3688
Unknown <jarb95@gmail.com>
2018-04-02 13:08:01 -0600

    Fix component declaration erros for proper prod environment building.

--- modulos-liderazgo/src/app/user-report/user-report.component.ts
+++ modulos-liderazgo/src/app/user-report/user-report.component.ts
@@ -6,6 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-report.component.css']
 })
 export class UserReportComponent implements OnInit {
+	
+	// ==========================================================================
+	// (COMPONENT IS UNUSED, VARS DECLARED TO AVOID PRODUCTION DEPLOYMENT ERRORS.)
+	// ==========================================================================
+	title: 		string;
+	closeBtnName: string;
+
+	// Chart arrays declaration.
+	radar: 		any[] = [];
+	barEvolucion: any[] = [];
+	barDias: 		any[] = [];
+	barIntentos: 	any[] = [];
+
+	nombre: string;
+	apellido: string;
+	puesto: string;
+	area: string;
+	departamento: string;
+	anos: string;
+
+	// Chart data arrays declaration
+	radarData: 		any[] = [];
+	barEvolucionData: any[] = [];
+	barDiasData: 		any[] = [];
+	barIntentosData: 	any[] = [];
+
+	appVars: any = []; 
+	bsModalRef: any = [];
 
   constructor() { }
 
